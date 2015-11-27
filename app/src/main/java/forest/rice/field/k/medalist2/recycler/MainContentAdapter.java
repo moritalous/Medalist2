b62db5725405cea1fced05de7201e33397dc3ebe
moritalous <moritalous@gmail.com>
2015-11-27 22:55:15 +0900

    Initial commit

---
+++ app/src/main/java/forest/rice/field/k/medalist2/recycler/MainContentAdapter.java
@@ -0,0 +1,3 @@
+package forest.rice.field.k.medalist2.recycler;
+
+public class MainContentAdapter{}
---
+++ app/src/main/java/forest/rice/field/k/medalist2/recycler/MainContentViewHolder.java
@@ -0,0 +1,7 @@
+package forest.rice.field.k.medalist2.recycler;
+
+/**
+ * Created by kazuaki on 2015/11/27.
+ */
+public class MainContentViewHolder {
+}
